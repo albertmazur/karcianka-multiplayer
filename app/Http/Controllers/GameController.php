c1005b4ccf139f8ab36a3dcf45675f6126136f4a
Albert Mazur <albert153@interia.pl>
2023-10-26 14:09:12 +0200

    add file

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class GameController extends Controller
+{
+    public function start(): View
+    {
+        return view('game.index');
+    }
+}
