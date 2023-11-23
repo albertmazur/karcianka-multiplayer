ec5feefca3b4f4fe71ead272fa61e97075bd3639
Albert Mazur <albert153@interia.pl>
2023-11-23 09:47:27 +0100

    change color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,20 +15,19 @@
         @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-cyan-950">
+        <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-cyan-700 shadow">
+                <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
             @endif
-
-            <!-- Page Content -->
             @include("layouts.shaderd.messages")
+            <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
