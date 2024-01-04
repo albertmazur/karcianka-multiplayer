6793bc35d106bdadfea597a82b4c2d0835099472
Albert Mazur <albert153@interia.pl>
2024-01-04 01:12:36 +0100

    ruls

--- lang/pl/game.php
+++ lang/pl/game.php
@@ -8,7 +8,21 @@ return[
     "show_cards" => "Pokaż katy",
     "watting" => "Czekanie na znajomego",
     "not_game" => "Nie ma gier",
+    "rul" => "Zasady gry",
     "ruls" => [
-
+        "1" => "podczas rozgrywki wykładać można 1 kartę. Karty na środek dokłada się według zasady koloru i figury. Jeśli więc na stole leży 5 kier, gracz może położyć dowolną 5 lub dowolnego kiera. Wyjątkiem są karty funkcyjne, które wymuszają wyłożenie na stół konkretnych figur.",
+        "2" => "kiedy gracz nie ma w dłoni karty, którą może wyłożyć na stół, dobiera jedną z kupki i traci kolejkę. Karty dobiera też, jeśli zbiera karę. Część z kart funkcyjnych określa, ile kart powinno się dobrać. Jeśli karta karna zostaje przebita kolejną, liczba kart w karze zwiększa się. Kara kumuluje się, póki któryś z graczy nie musi dobrać karty z kupki – wtedy ponosi karę, dobierając z talii skumulowaną w karze liczbę kart.",
+        "3" => "zmuszają gracza do dobrania tylu kart, ile na niej widnieje (Król 5 kart). Można przebijać wartości dobieranych kart tylko z kart bitewnych jeśli pasuje figura lub kolor.",
+        "4" => "Zmniejsza ilość kart do zebrania o 5.",
+        "5" => "kończy bitwę bez brania kart.",
+        "6" => "przenosi kart do zebrania na następnego gracza.",
+        "7" => "4, 5, 6, 7, 8, 9, 10.",
+        "dealing_cards" => "Wykładanie kart",
+        "drawing_cards" => "Dobieranie kart",
+        "battle_cards" => "Karty bitewne (dwójki, trójki i króle)",
+        "jack" => "Walet",
+        "lady" => "Dama",
+        "as" => "As",
+        "not_battle_cards" => "Karty nie funkcyjne",
     ]
 ];
--- resources/views/game/ruls.blade.php
+++ resources/views/game/ruls.blade.php
@@ -1,13 +1,13 @@
 <div class="mt-4 p-4">
-    <h1 class="mb-5">Zasady gry</h1>
+    <h2 class="text-4xl mb-5">{{__("game.rul")}}</h2>
     <ul>
-        <li><span>Wykładanie kart</span> – podczas rozgrywki wykładać można 1 kartę. Karty na środek dokłada się według zasady koloru i figury. Jeśli więc na stole leży 5 kier, gracz może położyć dowolną 5 lub dowolnego kiera. Wyjątkiem są karty funkcyjne, które wymuszają wyłożenie na stół konkretnych figur.</li>
-        <li><span>Dobieranie kart</span> – kiedy gracz nie ma w dłoni karty, którą może wyłożyć na stół, dobiera jedną z kupki i traci kolejkę. Karty dobiera też, jeśli zbiera karę. Część z kart funkcyjnych określa, ile kart powinno się dobrać. Jeśli karta karna zostaje przebita kolejną, liczba kart w karze zwiększa się. Kara kumuluje się, póki któryś z graczy nie musi dobrać karty z kupki – wtedy ponosi karę, dobierając z talii skumulowaną w karze liczbę kart.</li>
-        <li><span>Karty bitewne (dwójki, trójki i króle)</span> – zmuszają gracza do dobrania tylu kart, ile na niej widnieje (Król 5 kart). Można przebijać wartości dobieranych kart tylko z kart bitewnych jeśli pasuje figura lub kolor.</li>
-        <li><span>Walet</span> – Zmniejsza ilość kart do zebrania o 5.</li>
-        <li><span>Dama</span> – kończy bitwę bez brania kart.</li>
-        <li><span>As</span> – przenosi kart do zebrania na następnego gracza.</li>
-        <li><span>Karty nie funkcyjne</span> - 4, 5, 6, 7, 8, 9, 10.</li>
+        <li><span>{{__("game.ruls.dealing_cards")}}</span> – {{__("game.ruls.1")}}.</li>
+        <li><span>{{__("game.ruls.drawing_cards")}}</span> – {{__("game.ruls.2")}}</li>
+        <li><span>{{__("game.ruls.battle_cards")}}</span> – {{__("game.ruls.3")}}.</li>
+        <li><span>{{__("game.ruls.jack")}}</span> – {{__("game.ruls.4")}}</li>
+        <li><span>{{__("game.ruls.lady")}}</span> – {{__("game.ruls.5")}}</li>
+        <li><span>{{__("game.ruls.as")}}</span> – {{__("game.ruls.6")}}</li>
+        <li><span>{{__("game.ruls.not_battle_cards")}}</span> - {{__("game.ruls.7")}}</li>
     </ul>
 </div>
 <div class="stopka">
